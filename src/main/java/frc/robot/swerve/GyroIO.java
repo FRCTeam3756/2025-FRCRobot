b08bee907e119680181eb08573d26b6079159e80
Gabe3L <gabe3lynch@gmail.com>
2025-03-14 20:55:19 -0400

    Removed extra gyro logging - robot drives

--- src/main/java/frc/robot/swerve/GyroIO.java
+++ src/main/java/frc/robot/swerve/GyroIO.java
@@ -4,13 +4,13 @@
 
 package frc.robot.swerve;
 
-import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.StatusCode;
+// import com.ctre.phoenix6.BaseStatusSignal;
+// import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
+// import edu.wpi.first.math.geometry.Rotation2d;
+// import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import frc.robot.constants.SwerveConstants;
@@ -29,11 +29,5 @@ public class GyroIO {
     yaw.setUpdateFrequency(Drive.ODOMETRY_FREQUENCY);
     yawVelocity.setUpdateFrequency(50.0);
     pigeon.optimizeBusUtilization();
-  }
-
-  public void updateInputs(GyroIOInputs inputs) {
-    inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK);
-    inputs.yawPosition = Rotation2d.fromDegrees(yaw.getValueAsDouble());
-    inputs.yawVelocityRadPerSec = Units.degreesToRadians(yawVelocity.getValueAsDouble());
   }
 }
