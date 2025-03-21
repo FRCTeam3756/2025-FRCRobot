5d4c18bc692f4d4ee37daf5d6c5f7f7871b9f60c
Gabe3L <gabe3lynch@gmail.com>
2025-03-21 10:43:44 -0400

    Increased wrist movement speed. Claw works

--- src/main/java/frc/robot/constants/ClawConstants.java
+++ src/main/java/frc/robot/constants/ClawConstants.java
@@ -7,8 +7,8 @@ package frc.robot.constants;
 public class ClawConstants {
     public static final int MOTOR_MAX_AMPERAGE = 30;
 
-    public static final double WRIST_UP_SPEED = 0.14;
-    public static final double WRIST_DOWN_SPEED = -0.10;
+    public static final double WRIST_UP_SPEED = 0.20;
+    public static final double WRIST_DOWN_SPEED = -0.15;
 
     public static final double L1_SHOOT_SPEED = Integer.MAX_VALUE;
     public static final double L2_SHOOT_SPEED = Integer.MAX_VALUE;
@@ -17,5 +17,5 @@ public class ClawConstants {
     public static final double PROCESSOR_SHOOT_SPEED = Integer.MAX_VALUE;
 
     public static final double INTAKE_SPEED = 0.4;
-    public static final double OUTTAKE_SPEED = -0.6;
+    public static final double OUTTAKE_SPEED = -0.8;
 }
