5b46cd1d45927c4593b971049983355197ac6b67
Gabe Lynch <gabe3lynch@gmail.com>
2025-03-25 14:22:54 -0400

    Fixed auto and Added Pathplanners

--- src/main/java/frc/robot/auto/ClearReefAlgae.java
+++ src/main/java/frc/robot/auto/ClearReefAlgae.java
@@ -6,23 +6,24 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.*;
 
-public class ScoreCoralCenter extends SequentialCommandGroup {
+public class ClearReefAlgae extends SequentialCommandGroup {
     private final SwerveRequest.RobotCentric drive = new SwerveRequest.RobotCentric()
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
     private final SwerveRequest.Idle idle = new SwerveRequest.Idle();
 
-    public ScoreCoralCenter(CommandSwerveDrivetrain drivetrain, ElevatorSubsystem elevatorSubsystem, ClawSubsystem clawSubsystem) {
+    public ClearReefAlgae(CommandSwerveDrivetrain drivetrain, ElevatorSubsystem elevatorSubsystem, ClawSubsystem clawSubsystem) {
         addCommands(
             // Drive to the trough and raise elevator
             new ParallelCommandGroup(
-                drivetrain.applyRequest(() -> drive.withVelocityX(4.0)).withTimeout(0.5).andThen(drivetrain.applyRequest(() -> idle)),
+                drivetrain.applyRequest(() -> drive.withVelocityX(4.0)).withTimeout(0.4).andThen(drivetrain.applyRequest(() -> idle)),
                 new InstantCommand(() -> clawSubsystem.tiltWristDown(), clawSubsystem).withTimeout(0.2).andThen(() -> clawSubsystem.tiltWristStop(), clawSubsystem)
-                    .andThen(() -> elevatorSubsystem.elevatorUp(), elevatorSubsystem).withTimeout(1.5).andThen(() -> elevatorSubsystem.elevatorStop(), elevatorSubsystem)
+                    .andThen(() -> elevatorSubsystem.elevatorUp(), elevatorSubsystem).withTimeout(2.5).andThen(() -> elevatorSubsystem.elevatorStop(), elevatorSubsystem)
             ),
 
-            // Place the coral
+            // Pull the first algae off the reef
             new InstantCommand(() -> clawSubsystem.tiltWristDown(), clawSubsystem).withTimeout(0.8).andThen(() -> clawSubsystem.tiltWristStop(), clawSubsystem),
-            new InstantCommand(() -> clawSubsystem.outtakeRollers(), clawSubsystem).withTimeout(0.5).andThen(() -> clawSubsystem.stopRollers(), clawSubsystem),
+            drivetrain.applyRequest(() -> drive.withVelocityX(0.4)).withTimeout(1.0).andThen(drivetrain.applyRequest(() -> idle)),
+            new InstantCommand(() -> clawSubsystem.intakeRollers(), clawSubsystem).withTimeout(0.5).andThen(() -> clawSubsystem.stopRollers(), clawSubsystem),
             new InstantCommand(() -> clawSubsystem.tiltWristUp(), clawSubsystem).withTimeout(1.0).andThen(() -> clawSubsystem.tiltWristStop(), clawSubsystem),
 
             // Turn around while driving backwards and lower elevator
